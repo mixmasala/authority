9e866278ad542972b85fecbccd4adcfd635bd152
Yawning Angel <yawning@schwanenlied.me>
2017-11-08 21:19:29 +0000

    nonvoting: Validate signed descriptors.
    There's a dumb library limitation that requires a dumb hack to work
    around, and this needs more work in the descriptor validation area, but
    at least this works.

--- nonvoting/s11n.go
+++ nonvoting/s11n.go
@@ -17,14 +17,19 @@
 package nonvoting
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 
 	"github.com/katzenpost/core/crypto/eddsa"
 	"github.com/katzenpost/core/pki"
 	"gopkg.in/square/go-jose.v2"
 )
 
-const nodeDescriptorVersion = "nonvoting-v0"
+const (
+	nodeDescriptorVersion = "nonvoting-v0"
+	nodeJosePubKeyHdr     = "EdDSAPublicKey"
+)
 
 type nodeDescriptor struct {
 	// Version uniquely identifies the descriptor format as being for the
@@ -47,11 +52,20 @@ func signDescriptor(signingKey *eddsa.PrivateKey, base *pki.MixDescriptor) (stri
 	}
 
 	// Sign the descriptor.
+	//
+	// HACK: Frustratingly enough, the library doesn't appear to support
+	// embedding JWKs for EdDSA signatures, so just jam the public key into
+	// the header.
 	k := jose.SigningKey{
 		Algorithm: jose.EdDSA,
 		Key:       *signingKey.InternalPtr(),
 	}
-	signer, err := jose.NewSigner(k, nil)
+	sOpts := &jose.SignerOptions{
+		ExtraHeaders: map[jose.HeaderKey]interface{}{
+			nodeJosePubKeyHdr: signingKey.PublicKey(),
+		},
+	}
+	signer, err := jose.NewSigner(k, sOpts)
 	if err != nil {
 		return "", err
 	}
@@ -62,4 +76,74 @@ func signDescriptor(signingKey *eddsa.PrivateKey, base *pki.MixDescriptor) (stri
 
 	// Serialize the key, descriptor and signature.
 	return signed.CompactSerialize()
+}
+
+func verifyAndParseDescriptor(epoch uint64, b []byte) (*pki.MixDescriptor, error) {
+	signed, err := jose.ParseSigned(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	// The descriptor should be signed by it's own key, but the library
+	// doesn't give a convenient way to extract the payload when it hasn't
+	// been verified, so reach into the header instead.
+	//
+	// HACK: This should be an embedded JWK, but it's just Base64 encoded
+	// into the JWS Header because of library limitations.
+	if len(signed.Signatures) != 1 {
+		return nil, fmt.Errorf("nonvoting: Expected 1 signature, got: %v", len(signed.Signatures))
+	}
+	alg := signed.Signatures[0].Header.Algorithm
+	if alg != "EdDSA" {
+		return nil, fmt.Errorf("nonvoting: Unsupported signature algorithm: '%v'", alg)
+	}
+	s, ok := signed.Signatures[0].Header.ExtraHeaders[nodeJosePubKeyHdr]
+	if !ok {
+		return nil, fmt.Errorf("nonvoting: Failed to find descriptor public key")
+	}
+	var candidatePk eddsa.PublicKey
+	pkStr, ok := s.(string)
+	if !ok {
+		return nil, fmt.Errorf("nonvoting: Pathologically malfored descriptor public key")
+	}
+	if err = candidatePk.UnmarshalText([]byte(pkStr)); err != nil {
+		return nil, err
+	}
+
+	// Verify that the descriptor is signed by the key in the header.
+	payload, err := signed.Verify(*candidatePk.InternalPtr())
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the payload.
+	d := new(nodeDescriptor)
+	if err = json.Unmarshal(payload, d); err != nil {
+		return nil, err
+	}
+
+	// Ensure the descriptor is well formed.
+	if d.Version != nodeDescriptorVersion {
+		return nil, fmt.Errorf("nonvoting: Invalid Descriptor Version: '%v'", d.Version)
+	}
+	if d.LinkKey == nil {
+		return nil, fmt.Errorf("nonvoting: Descriptor missing 'LinkKey'")
+	}
+	if d.IdentityKey == nil {
+		return nil, fmt.Errorf("nonvoting: Descriptor missing 'IdentityKey'")
+	}
+	// XXX: Addresses
+	// XXX: Layer
+	// XXX: MixKeys (Use `epoch`).
+
+	// And as the final check, ensure that the key embedded in the descriptor
+	// matches the key embedded in the JOSE header, that we used to validate
+	// the signature.
+	//
+	// MixDescriptors returned from this function are essentially known to be
+	// "well formed", and correctly self-signed.
+	if !bytes.Equal(candidatePk.Bytes(), d.IdentityKey.Bytes()) {
+		return nil, fmt.Errorf("nonvoting: Descriptor signing key mismatch")
+	}
+	return &d.MixDescriptor, nil
 }
